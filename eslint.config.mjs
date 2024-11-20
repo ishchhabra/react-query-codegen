211cd9a5948f548853cf4b27de8de848f1871757
Ish Chhabra <ishchhabra12@gmail.com>
2024-11-20 19:43:27 +0530

    update eslint configuration to handle jest and node globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import eslint from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tseslintParser from "@typescript-eslint/parser";
+import jest from "eslint-plugin-jest";
+import globals from "globals";
 
 export default [
   eslint.configs.recommended,
@@ -14,6 +16,22 @@ export default [
     },
     rules: {
       ...tseslint.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.test.ts", "**/*.test.tsx"],
+    languageOptions: {
+      parser: tseslintParser,
+      globals: {
+        ...globals.node,
+        ...jest.environments.globals.globals,
+      },
+    },
+    plugins: {
+      jest,
+    },
+    rules: {
+      ...jest.configs.recommended.rules,
     },
   },
 ];
