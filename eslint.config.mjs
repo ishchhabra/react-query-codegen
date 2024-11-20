ef197dd7e204218090e4575518a6637df34e9274
Ish Chhabra <ishchhabra12@gmail.com>
2024-11-20 19:17:35 +0530

    Implement CI

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tseslintParser from "@typescript-eslint/parser";
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tseslintParser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+];
